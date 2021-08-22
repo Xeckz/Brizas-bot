1f0035b3da4af4fd377448aeebeb93893c1dc30d
Xeckz <89345866+Xeckz@users.noreply.github.com>
2021-08-22 09:29:01 -0500

    Xeckz

---
+++ welcometext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚𝐦 @${number}. 𝕭𝖎𝖊𝖓𝖛𝖊𝖓𝖎𝖉𝖔 𝖆𝖑 𝖌𝖗𝖚𝖕𝖔 ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `𝐀𝐬𝐭𝐚 𝐧𝐮𝐧𝐜𝐚 @${number}. 𝐉𝐨𝐭𝐨 𝐧𝐨 𝐭𝐞 𝐯𝐚𝐦𝐨𝐬 𝐚 𝐞𝐱𝐭𝐫𝐚𝐧̃𝐚𝐫 🖕🏻 👋`
+}
+exports.bye = bye
